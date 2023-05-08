a7771e534fd71216419bd744b05c6687eca7fc6d
krleong <krleong1234@gmail.com>
2023-05-08 01:30:21 -0700

    Added comments to frontend/games files and updated gitignore

--- frontend/games/created.js
+++ frontend/games/created.js
@@ -1,8 +1,10 @@
+// This file shows a list of created games
 import { GAME_CREATED } from "../../shared/constants";
 
 const gameList = document.querySelector("#game-list");
 const itemTemplate = document.querySelector("#available-game-item");
 
+// Displays list of available games to join
 export function createGameListItem(game_id, _created_at) {
   const entry = itemTemplate.content.cloneNode(true);
 
@@ -12,6 +14,7 @@ export function createGameListItem(game_id, _created_at) {
   return entry;
 }
 
+// Listens for new games being created
 export function gameCreatedHandler(socket) {
   socket.on(GAME_CREATED, ({ game_id, created_at }) => {
     gameList.appendChild(createGameListItem(game_id, created_at));
--- frontend/games/get-game-id.js
+++ frontend/games/get-game-id.js
@@ -2,9 +2,12 @@
  *
  * @param {String} location
  */
+
+// Grabs the game id from list of available games
 export function getGameId(location) {
   const gameId = location.substring(location.lastIndexOf("/") + 1);
 
+  // If the game id is "lobby", then it's the lobby page, otherwise it's a game
   if (gameId === "lobby") {
     return 0;
   } else {
