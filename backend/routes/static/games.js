41d1eff1a919695cd40874b5c5cefa84ccf9bd63
krleong <krleong1234@gmail.com>
2023-05-14 17:52:55 -0700

    Refactored and got rid of unneccessary views files

--- backend/routes/static/games.js
+++ backend/routes/static/games.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/:id", (req, res) => {
   const { id } = req.params;
 
-  res.render("games", { id, title: "Team Ducling's term project" });
+  res.render("game-session", { id, title: "Team Ducling's term project" });
 });
 
 module.exports = router;
